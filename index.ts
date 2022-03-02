d19af5fb5834eef8ddf7b7c62388e935dac4be1d
joel <joelnunez855@gmail.com>
2022-03-02 09:35:31 -0300

    segundo commit

--- index.ts
+++ index.ts
@@ -49,32 +49,33 @@ class Edificio {
 }
 // no modificar este test
 function testClaseEdificio() {
-    const unPiso = new Piso("planta baja");
-    const otroPiso = new Piso("primer piso");
-    const unEdificio = new Edificio([unPiso, otroPiso]);
-    const deptoUno = new Departamento("depto uno");
-    const deptoDos = new Departamento("depto dos");
-    const deptoTres = new Departamento("depto tres");
-    unEdificio.addDepartamentoToPiso("planta baja", deptoUno);
-    unEdificio.addDepartamentoToPiso("planta baja", deptoDos);
-    unEdificio.addDepartamentoToPiso("planta baja", deptoTres);
-  
-    const deptos = unEdificio.getDepartamentoByPiso("planta baja");
-    const deptosEmpty = unEdificio.getDepartamentoByPiso("primer piso");
-  
-    if (
-      Array.isArray(deptosEmpty) &&
-      deptosEmpty.length == 0 &&
-      deptos.length == 3 &&
-      deptos[2].getName() == "depto tres"
-    ) {
-      console.log("testClaseBandaApartment passed");
-    } else {
-      throw "testClaseBandaApartment not passed";
-    }
+  const unPiso = new Piso('planta baja');
+  const otroPiso = new Piso('primer piso');
+  const unEdificio = new Edificio([unPiso, otroPiso]);
+  const deptoUno = new Departamento('depto uno');
+  const deptoDos = new Departamento('depto dos');
+  const deptoTres = new Departamento('depto tres');
+  unEdificio.addDepartamentoToPiso('planta baja', deptoUno);
+  unEdificio.addDepartamentoToPiso('planta baja', deptoDos);
+  unEdificio.addDepartamentoToPiso('planta baja', deptoTres);
+
+  const deptos = unEdificio.getDepartamentoByPiso('planta baja');
+  const deptosEmpty = unEdificio.getDepartamentoByPiso('primer piso');
+
+  if (
+    Array.isArray(deptosEmpty) &&
+    deptosEmpty.length == 0 &&
+    deptos.length == 3 &&
+    deptos[2].getName() == 'depto tres'
+  ) {
+    console.log('testClaseBandaApartment passed');
+  } else {
+    throw 'testClaseBandaApartment not passed';
   }
-  
-  function main() {
-    testClaseEdificio();
-  }
-  main();
+}
+
+function main() {
+  testClaseEdificio();
+  console.log('cambiazo');
+}
+main();
